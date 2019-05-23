885b57cf4ce3d7d8625a3858ab34081cb859d645
Hanna Hoshino <starfield1019@gmail.com>
2019-05-23 21:00:51 +0900

    Initial commit

---
+++ net_comparison/model/lenet.py
@@ -0,0 +1,36 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+IMG_WIDTH = 32
+IMG_HEIGHT = 32
+COLOR_CHANNEL = 3
+
+
+class LeNet5(nn.Module):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+
+        self.fc1 = nn.Linear(5*5*16, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+
+        return out
---
+++ net_comparison/model/mobilenet_v1.py
@@ -0,0 +1,95 @@
+import torch.nn as nn
+
+
+IMG_WIDTH = 32
+IMG_HEIGHT = 32
+COLOR_CHANNEL = 3
+
+
+class DepthwiseBlock(nn.Module):
+    def __init__(self, in_channels, kernel_size, stride=1, padding=1, bias=False):
+        super(DepthwiseBlock, self).__init__()
+        self.dw_block = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels, kernel_size, stride=stride,
+                      padding=padding, groups=in_channels, bias=bias),
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        out = self.dw_block(x)
+
+        return out
+
+
+class OneByOneBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, padding=1, bias=False):
+        super(OneByOneBlock, self).__init__()
+        self.one_by_one_block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1,
+                      stride=stride, padding=padding, bias=bias),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        out = self.one_by_one_block(x)
+
+        return out
+
+
+class MobileNetV1(nn.Module):
+    def __init__(self):
+        super(MobileNetV1, self).__init__()
+
+        self.conv_bn = nn.Sequential(
+            nn.Conv2d(IMG_HEIGHT*IMG_WIDTH*COLOR_CHANNEL,
+                      32, kernel_size=3, stride=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+
+            DepthwiseBlock(32, kernel_size=3),
+            OneByOneBlock(32, 64),
+
+            DepthwiseBlock(64, kernel_size=3, stride=2),
+            OneByOneBlock(64, 128),
+
+            DepthwiseBlock(128, kernel_size=3),
+            OneByOneBlock(128, 128),
+
+            DepthwiseBlock(128, kernel_size=3, stride=2),
+            OneByOneBlock(128, 256),
+
+            DepthwiseBlock(256, kernel_size=3),
+            OneByOneBlock(256, 256),
+
+            DepthwiseBlock(256, kernel_size=3, stride=2),
+            OneByOneBlock(256, 512),
+
+            DepthwiseBlock(512, kernel_size=3),
+            OneByOneBlock(512, 512),
+            DepthwiseBlock(512, kernel_size=3),
+            OneByOneBlock(512, 512),
+            DepthwiseBlock(512, kernel_size=3),
+            OneByOneBlock(512, 512),
+            DepthwiseBlock(512, kernel_size=3),
+            OneByOneBlock(512, 512),
+            DepthwiseBlock(512, kernel_size=3),
+            OneByOneBlock(512, 512),
+
+            DepthwiseBlock(512, kernel_size=3),
+            OneByOneBlock(512, 1024),
+
+            DepthwiseBlock(1024, kernel_size=3, stride=2),
+            OneByOneBlock(1024, 1024),
+            nn.AvgPool2d(7),
+        )
+
+        self.fc = nn.Linear(1024, 1000)
+
+    def forward(self, x):
+        out = self.conv_bn(x)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+
+        return out
